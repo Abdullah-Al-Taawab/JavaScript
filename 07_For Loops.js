3e5769776b0b949c9b2de56e5972f62e032bc4ee
Abdullah Al Taawab <abdullah.taawab@sonaliintellect.com>
2023-01-15 11:31:23 +0600

    07_Foor Loops modified

--- 07_For Loops.js
+++ 07_For Loops.js
@@ -7,3 +7,26 @@ for(let i=1;i<=n;i++)
   }
 console.log(sum)
 console.log(sum<20? "sum is less than 20":"sum is greater than 20");
+
+let obj=
+  {
+    taawab:"18101011",
+    anik:"20341043",
+    rapheo:"18101010"
+  }
+for(let b in obj)
+  {
+    console.log("id of " + b + " is "+obj[b]);
+  }
+
+for(let c in "anik")
+{
+  console.log(c);
+  
+}
+for(let c of "anik")
+{
+  console.log(c);
+  
+}
+
