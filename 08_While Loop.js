bdc5a1fa115b91d67256a7f254acf836b87cb682
Abdullah Al Taawab <abdullah.taawab@sonaliintellect.com>
2023-01-15 15:37:43 +0600

    function and while loop added

---
+++ 08_While Loop.js
@@ -0,0 +1,18 @@
+let a = prompt("Enter a number");
+a=Number.parseInt(a)
+let i=0;
+while(i<a)
+  {
+    console.log(" i = " + i);
+    i++;
+  }
+console.log(" i is now greater or equal to A");
+
+let b = prompt("Enter a number");
+b=Number.parseInt(b)
+let p=100;
+do{ // in "do-while" loop atleast one time will execute.
+  p--;
+  console.log("P is greater than b")
+}while(p>b)
+
