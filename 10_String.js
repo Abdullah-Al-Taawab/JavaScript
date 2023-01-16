e80484fda8112da3d742c9ae33baa4f46dab7bd2
Abdullah Al Taawab <abdullah.taawab@sonaliintellect.com>
2023-01-16 12:18:54 +0600

    String and String methods added

---
+++ 10_String.js
@@ -0,0 +1,10 @@
+let name="taawab"
+console.log(name.length);
+console.log(name[1]);
+let name1='anik'
+console.log(name1)
+//template literals use backticks instead of qoutes (`` back tick)
+let course1="cse110";
+let course2="cse111";
+let courses=`${course1} and ${course2}`;
+console.log(courses);
