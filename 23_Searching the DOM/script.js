30e7f5dd2894442f8cdee7e7159f11154b8157e3
Abdullah Al Taawab <abdullahaltaawab@gmail.com>
2023-01-31 16:04:20 +0600

    file 23 added

---
+++ 23_Searching the DOM/script.js
@@ -0,0 +1,5 @@
+let title1 = document.getElementsByClassName("card-title")[1]; // it returns all elements with this class name so we have to specify the exact one.
+title1.style.color = "red";
+let title0 = document.getElementById("first");
+title0.style.color = "blue";
+console.log(document.getElementsByTagName("a"))
